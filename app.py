75b149bb03017df5ff4829888b735c6c4d66a29d
jayanta8509 <jayantameslova@gmail.com>
2025-12-19 21:13:49 +0530

    chore: Update uvicorn server port from 8002 to 8000

--- app.py
+++ app.py
@@ -216,7 +216,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
-        port=8002,
+        port=8000,
         reload=True,
         log_level="info"
     )
